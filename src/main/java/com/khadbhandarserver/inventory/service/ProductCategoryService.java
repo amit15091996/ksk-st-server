eab6f65fc16ce96b3d48d5e7a26d084838bf5695
PRIYABRAT PANDA <121928016+PRIYABRAT0707@users.noreply.github.com>
2024-03-28 19:17:04 +0530

    stock report changes

--- src/main/java/com/khadbhandarserver/inventory/service/ProductCategoryService.java
+++ src/main/java/com/khadbhandarserver/inventory/service/ProductCategoryService.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public interface ProductCategoryService {
 
-	Map<Object, Object> getCategoryWiseData(String CategoryName);
+	Map<Object, Object> getCategoryWiseData(String CategoryName,String month,String fromDate,String toDate);
 
 	Map<Object, Object> getAllCategory();
 
