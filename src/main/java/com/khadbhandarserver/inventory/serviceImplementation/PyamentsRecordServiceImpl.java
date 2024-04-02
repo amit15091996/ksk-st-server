d713794ecb5beef99a67b2bf93218fbaa64fae62
PRIYABRAT PANDA <121928016+PRIYABRAT0707@users.noreply.github.com>
2024-04-02 19:01:05 +0530

    payment changes

--- src/main/java/com/khadbhandarserver/inventory/serviceImplementation/PyamentsRecordServiceImpl.java
+++ src/main/java/com/khadbhandarserver/inventory/serviceImplementation/PyamentsRecordServiceImpl.java
@@ -8,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.khadbhandarserver.inventory.dto.PyamentsRecordDto;
+import com.khadbhandarserver.inventory.entity.PurchaseRecord;
 import com.khadbhandarserver.inventory.entity.PyamentsRecord;
 import com.khadbhandarserver.inventory.exception.BadRequest;
 import com.khadbhandarserver.inventory.exception.NotFoundException;
 import com.khadbhandarserver.inventory.helper.AppConstant;
+import com.khadbhandarserver.inventory.repository.PurchaseRecordRepository;
 import com.khadbhandarserver.inventory.repository.PyamentsRecordRepository;
 import com.khadbhandarserver.inventory.service.PyamentsRecordService;
 
@@ -22,16 +24,22 @@ public class PyamentsRecordServiceImpl implements PyamentsRecordService {
 	@Autowired
 	private PyamentsRecordRepository pyamentsRecordRepository;
 
+	@Autowired
+	private PurchaseRecordRepository purchaseRecordRepository;
+	
 	@Override
 	public Map<Object, Object> insertPaymentRecord(Long purchasedItemId, PyamentsRecordDto pyamentsRecordDto) {
 		Map<Object, Object> paymentRecordMap = new HashMap<>();
-		   PyamentsRecord pyamentsRecord = new PyamentsRecord();
+		Optional<PurchaseRecord> purchaseRecord=this.purchaseRecordRepository.findById(purchasedItemId);
+		
+		if(purchaseRecord.isPresent()) {
+
+		     PyamentsRecord pyamentsRecord = new PyamentsRecord();
 			pyamentsRecord.setPayeeName(pyamentsRecordDto.getPayeeName());
 			pyamentsRecord.setPaymentDate(pyamentsRecordDto.getPaymentDate());
 			pyamentsRecord.setPaidProductGroup(pyamentsRecordDto.getPaidProductGroup());
 			pyamentsRecord.setPaymentAmount(pyamentsRecordDto.getPaymentAmountPerUnit());
-			
-
+			pyamentsRecord.setPurchaseRecord(purchaseRecord.get());
 			try {
 				PyamentsRecord PyamentsRecordSaved = this.pyamentsRecordRepository.save(pyamentsRecord);
 				if (PyamentsRecordSaved != null) {
@@ -42,6 +50,11 @@ public class PyamentsRecordServiceImpl implements PyamentsRecordService {
 			} catch (Exception e) {
 				throw new BadRequest(e.getMessage());
 			}
+		}
+		else {
+			throw new NotFoundException(AppConstant.noRecordFound+" "+purchasedItemId);
+		}
+		
 
 		
 		return paymentRecordMap;
